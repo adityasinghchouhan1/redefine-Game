3bfbf4c8e73d8865152956a9866e740e41d887ba
aditya <asadityasingh2572000@gmail.com>
2025-03-19 22:02:19 +0530

    imp

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import About from './Components/About'
 import Navbar from './Components/Navbar'
 import Features from './Components/Features'
 import Story from './Components/Story'
-
+import Contect from './Components/Contect'
+import Footer from './Components/Footer'
 const App = () => {
   return (
     <main className=" relative min-h-screen w-screen overflow-x-hidden">
@@ -13,6 +14,8 @@ const App = () => {
       <About />
       <Features />
       <Story />
+      <Contect />
+      <Footer />
     </main>
   )
 }
